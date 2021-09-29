0f1519cd19b5fedce7b8b0bc8c4fad4009520c15
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-09-29 02:10:03 +0000

    Cannot be interactive in a crontab.

--- docker-resume-builder.sh
+++ docker-resume-builder.sh
@@ -4,6 +4,6 @@
 ## sudo docker run -i -d  -v '/root/Dylan-Resume:/source' -v '/var/www/files.dylanmtaylor.com/html:/destination' --name resume-builder --restart always ubuntu:latest
 
 # First we isntall texlive build dependencies if they're not there already.
-sudo docker exec -it resume-builder bash -c 'apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install -y texlive-xetex texlive-fonts-recommended texlive-font-utils'
+sudo docker exec -t resume-builder bash -c 'apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get install -y texlive-xetex texlive-fonts-recommended texlive-font-utils'
 
-sudo docker exec -it resume-builder bash -c 'cd /source &&  xelatex --output-directory=/destination dylan-resume.tex; ls -lah /destination/'
+sudo docker exec -t resume-builder bash -c 'cd /source &&  xelatex --output-directory=/destination dylan-resume.tex; ls -lah /destination/'
